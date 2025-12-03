0b24de3600d151069b1b8b01f3d3f2867c110d06
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-12-03 13:54:42 +0800

    Components, props, jsx etc.

--- 03-pizza-menu/src/App.jsx
+++ 03-pizza-menu/src/App.jsx
@@ -1,11 +1,102 @@
-import React from 'react'
+import React from 'react';
+import "./App.css";
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 
 const App = () => {
   return (
-    <div>
-      <h1 class="font-bold">Proejct creation.</h1>
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   )
 }
 
 export default App
+
+function Pizza(props) {
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className='header'>
+      <h1 className=''>Fast React Pizza Co.</h1>;
+    </header>
+  )
+}
+
+function Menu() {
+  return (
+  <main className='menu'>
+    <h2 className='text-xl font-medium'>Our Menu</h2>
+    <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, ham, aragula, and burrata cheese' photoName='pizzas/spinaci.jpg' price={10}/>
+    <Pizza name='Pizza Funghi' ingredients='Tomato, mozarella' photoName='pizzas/funghi.jpg' price={12}/>
+  </main>
+  )
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return <footer className='footer'>{new Date().toLocaleTimeString()} We're currently open!</footer>
+
+}
